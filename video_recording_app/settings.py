ab15ccad1c5bb5491edfb9610d4450e22c612e7d
Augustine Okeke <hursteen@gmail.com>
2019-05-09 15:31:55 +0100

    added selfvid into allowed host

--- video_recording_app/settings.py
+++ video_recording_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jvow)@2!#qk8%$u6(7e%wnw#btc^-)da*-(e&5av221aray3ks'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quick-loan.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['selfvid.herokuapp.com', '127.0.0.1', 'localhost']
 
 AUTH_USER_MODEL = 'accounts.User'
 
